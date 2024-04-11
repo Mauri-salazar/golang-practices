b929abd5605c6bc1a603c2cadb00f1eccf3cae4e
Mauri-salazar <salazar.mauricio.dev@gmail.com>
2024-04-11 13:25:30 -0300

    exercise  number one  goroutines-concurrence Ninja9

--- Exercise1-Nj9/main.go
+++ Exercise1-Nj9/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
 
+var wg sync.WaitGroup
+
+func main() {
+	fmt.Println("CPUs\t\t\t", runtime.NumCPU())           // I get the numbers CPUs
+	fmt.Println("Goroutines\t\t", runtime.NumGoroutine()) // I get the numbers of goroutines in execution.
+
+	wg.Add(2)
+	go bar()
+	go foo()
+
+	fmt.Println("System operator\t\t", runtime.GOOS) // I get the system operator of my computer.
+	fmt.Println("ARCH\t\t", runtime.GOARCH)          // I get the Architecture of system.
+
+	fmt.Println("CPUs\t\t\t", runtime.NumCPU())           // I get the numbers CPUs
+	fmt.Println("Goroutines\t\t", runtime.NumGoroutine()) // I get the numbers of goroutines in execution.
+
+	wg.Wait()
+
+	fmt.Println("Finish the programer")
+	fmt.Println("CPUs\t\t\t", runtime.NumCPU())           // I get the numbers CPUs
+	fmt.Println("Goroutines\t\t", runtime.NumGoroutine()) // I get the numbers of goroutines in execution.
+
+}
+
+func bar() {
+	fmt.Println("Printing from bar")
+	wg.Done()
+}
+
+func foo() {
+	fmt.Println("Printing from foo")
+	wg.Done()
 }
