a3a3adaef50ce7d75e78db0d0e91132802d16918
Mauri-salazar <salazar.mauricio.dev@gmail.com>
2024-05-02 09:38:57 -0300

    e.g go-doc

---
+++ go-doc-e-g/mymath.go
@@ -0,0 +1,13 @@
+//Package ejemplo proporciona funciones para realizar operaciones matemáticas básicas.
+
+package mymath
+
+// Sumar suma dos números enteros y devuelve el resultado.
+func Add(a, b int) int {
+	return a + b
+}
+
+// Restar resta dos números enteros y devuelve el resultado.
+func Subtract(a, b int) int {
+	return a - b
+}
