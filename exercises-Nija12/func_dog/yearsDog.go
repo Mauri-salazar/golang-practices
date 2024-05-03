8209d6d9500a9376e4732c1a81c9b59700d2e94c
Mauri-salazar <salazar.mauricio.dev@gmail.com>
2024-05-03 08:23:47 -0300

    ex1 nj12 pkg yearsDog.go  with documentation

---
+++ exercises-Nija12/func_dog/yearsDog.go
@@ -0,0 +1,14 @@
+// convert the human years to dog years
+package yearsDog
+
+// multiply human year * dog year
+func yearsDog(a int) int {
+	yearsDog := 7
+
+	//condition
+	if a == 1 {
+		return yearsDog
+	} else {
+		return a * yearsDog
+	}
+}
